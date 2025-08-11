405358031e6af6be8c4081c04961f64f94dbd9d9
“sankalpprasad” <sankalp.prasad.in@gmail.com>
2025-08-11 06:24:54 -0400

    Module 4a

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -6,7 +6,7 @@ const pinoLogger = require('./logger');
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
-
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
 app.use("*",cors());
@@ -41,7 +41,7 @@ app.use('/api/gifts', giftRoutes);
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/gifts', searchRoutes);
 
-
+app.use('/api/auth', authRoutes);
 
 // Global Error Handler
 app.use((err, req, res, next) => {
